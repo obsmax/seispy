56f138fa38c9b469bf81a038349e517041e5485a
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-09-04 11:16:16 +0200

    improve __init__

--- seispy/__init__.py
+++ seispy/__init__.py
@@ -1,5 +1,8 @@
 import sys, glob, os
 import numpy as np
 import matplotlib.pyplot as plt
+from seispy.version import __version__
 from seispy.trace import Trace
 from seispy.stream import Stream, readseispystream
+from seispy.time.utc import UTC, UTCFromTimestamp, UTCFromStr
+from seispy.time.timetick import timetick
