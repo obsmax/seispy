63e8e2c1777248136e66ae5471760cd25701b46e
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-10-17 13:35:32 +0200

    align with timetools for version policy

--- seispy/__init__.py
+++ seispy/__init__.py
@@ -1,11 +1,3 @@
-import sys, glob, os
-import numpy as np
-import matplotlib.pyplot as plt
-
-# fuck distutils2
-version_file = os.path.join(os.path.dirname(__file__), 'version.txt')
-with open(version_file, "r") as fh:
-    __version__ = fh.read().rstrip('\n')
-
+from seispy.version import __version__
 from seispy.trace import Trace
 from seispy.stream import Stream, readseispystream
--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 import setuptools, os
 
 # fuck distutils2
-version_file = os.path.join('seispy', 'version.txt')
-with open(version_file, "r") as fh:
-    __version__ = fh.read().rstrip('\n')
+version_file = os.path.join('seispy', 'version.py')
+if not os.path.isfile(version_file):
+    raise IOError(version_file)
+
+with open(version_file, "r") as fid:
+    for l in fid:
+        if l.strip('\n').strip().startswith('__version__'):
+            __version__ = l.strip('\n').split('=')[-1].split()[0].strip()
+            break
+    else:
+        raise Exception(f'could not detect __version__ affectation in {version_file}')
 
 with open("Readme.md", "r") as fh:
     long_description = fh.read()
@@ -23,6 +31,6 @@ setuptools.setup(
         "Operating System :: Linux"],
     python_requires='>=3.7',
     install_requires=[
-        'timetools @ git+https://gitlab.com/obsmax/timetools.git@v0.0.2#egg=timetools',
+        'timetools @ git+https://gitlab.com/obsmax/timetools.git@v0.0.3#egg=timetools',
         'numpy', 'scipy', 'matplotlib'],
     scripts=[os.path.join("seispy", "bin", "viz"), ])
